e920ba1915f924def6e0f5fd8c8dc37943de8fc6
ShrivastavaAstha <116484958+ShrivastavaAstha@users.noreply.github.com>
2022-12-08 09:04:24 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,21 @@
+let a = ["Post 1", "Post 2","Post 3","Post 4","Post 5"];
+
+//accessing values of array
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+//adding an element in array
+a.push("Post 6");
+a.push("Push 7");
+console.log(a[5]);
+console.log(a[6]);
+
+//removing an element from array
+a.pop();
+console.log(a);
+
+//getting length of array
+console.log(a.length);
---
+++ loops.js
@@ -0,0 +1,4 @@
+for (let index = 0; index < 10; index = index + 1)
+console.log("He is out of the city");
+
+console.log("Looping completed and came out of For loop bracket");
