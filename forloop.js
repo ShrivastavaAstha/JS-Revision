32c7ae62b19ee1108585813b4c95282e74efd767
ShrivastavaAstha <116484958+ShrivastavaAstha@users.noreply.github.com>
2022-12-09 22:37:35 +0530

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,31 @@
+for (let index = 0; index < 7; index = index + 1) {
+    console.log("the current value of index is", index);
+}
+
+console.log("Hello after the loop");
+
+//uses of for loop
+//1. to print/access all the elements of array  
+
+let a = ["Post 1", "Post 2", "Post 3", "Post 4", "Post 5", "Post 6", "Post 7"];
+//console.log(a.length);
+for (let index = 0; index < a.length; index++){
+    console.log("The element at ", index, "position of array is", a[index]);
+}
+//whenever we want to print a sentence that consists of one or more variables
+//1.then we separate them by commas 
+//2.use template literals
+//Rule 1=>`` to enclose whole sentence
+//Put variables inside ${variable}
+
+
+//let staticSentence = "Hello this is ASTHA!";
+//console.log(staticSentence);
+
+let dynamicSentence = "Hello I am from ";
+let collegename = "MMMUT";
+
+//console.log("Hello I am from ", collegename);
+
+//another way
+console.log(`Hello Iam from ${collegename}`);
