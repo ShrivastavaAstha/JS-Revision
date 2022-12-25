cafcbd906217cfd1179792bdbb7610ce0d28efa9
Astha <astha2333@gmail.com>
2022-12-25 22:18:41 +0530

    practice

---
+++ Practice4.js
@@ -0,0 +1,14 @@
+//Program for palindrome :
+
+function palindrome() {
+  const string = "racecar";
+  const len = string.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (string[i] !== string[len - 1 - i]) {
+      console.log("It is not a palindrome.");
+    }
+  }
+  console.log("It is a palindrome.");
+}
+
+palindrome();
